826896de89542828852dde42ffa8d4cb8809b0eb
vitalJr <vital_junior_@hotmail.com>
2020-07-20 17:22:42 +0100

    configuracao heroku

--- src/database.js
+++ src/database.js
@@ -4,16 +4,28 @@ class Database {
     constructor() {
         this.init();
     }
+    //Homologação
+    // init() {
+    //     mongoose.connect('mongodb://localhost:27017/userdb',
+    //         {
+    //             useNewUrlParser: true,
+    //             useUnifiedTopology: true,
+    //             useCreateIndex: true,
+    //             useFindAndModify: true,
+    //         },
+    //         console.log('Database Conectado'));
+    // }
 
+    //Produção
     init() {
-        mongoose.connect('mongodb://localhost:27017/userdb',
+        mongoose.connect('mongodb+srv://vitaljr:java170228@cluster0.wneoi.mongodb.net/<dbname>?retryWrites=true&w=majority',
             {
                 useNewUrlParser: true,
                 useUnifiedTopology: true,
                 useCreateIndex: true,
                 useFindAndModify: true,
             },
-            console.log('Database Conectado'));
+        )
     }
 }
 
